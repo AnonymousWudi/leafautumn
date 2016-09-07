ba5bf5b7c429c8bb934c081da513cd9e1e09a7ba
wudim <wudim@breadtrip.com>
2016-09-07 15:24:41 +0800

    2016-09-07

--- leafserver/scripts/default_database_value_add.py
+++ leafserver/scripts/default_database_value_add.py
@@ -1,22 +1,22 @@
+#!/usr/bin/env python
 # coding=utf-8
 
-from django.contrib.auth.models import Permission
-from leafautumn.leafautumn.functions import get_object_or_None
+import os
+import sys
 
-PERMISSION_LIST = [
-	('Can login dashboard', 10, 'login_dashboard')
-]
+pro_dir = os.getcwd()
+print pro_dir
+sys.path.append(pro_dir)
 
-def main():
-	for p in PERMISSION_LIST:
-		if get_object_or_None(Permission, name=p[0]):
-			continue
-		else:
-			permission = Permission()
-			permission.name            = p[0]
-			permission.content_type_id = p[1]
-			permission.codename        = p[2]
-			permission.save()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'leafautumn.settings'
+
+from models import Subject
+
+
+def add_default_subjects():
+
+    return True
+
 
 if __name__ == '__main__':
-	main()
+    add_default_subjects()
